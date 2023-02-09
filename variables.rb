85a0cd14f43383a2fbe228ca5bded096934a3834
Justin Pasko <jpasko1112@gmail.com>
2023-02-09 10:11:03 -0500

    Add 'conditionals' practice

--- variables.rb
+++ variables.rb
@@ -1,28 +1,6 @@
-## Strings
-
-name = "Justin"
-last_name = "pasko"
-
-## Integers
-
-age = 36
-dogs = 2
-kids = 2
-
-## Floats
-
-pi = 3.14
-cheese = 3.2
-
-## Booloeans
-
-hungry = true
-salty = false
-
-## New Strings
-
-name = "Walter"
-last_name = "Kronkite"
-
-puts "i like cheese"
-puts "You're a wizard Harry!"
+password = "cheesefingers"
+if password str.count >= 8
+    puts "nice"
+else
+    puts "nope"
+end
